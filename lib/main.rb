b639a1bd12c0be7ab8536b70832ac01c933b5028
Wladimir Gramacho <wladimirgramacho@gmail.com>
2021-09-16 19:53:34 -0300

    feat: only returns valid links

--- lib/main.rb
+++ lib/main.rb
@@ -8,8 +8,12 @@ def fetch(url)
   result = OpenStruct.new
 
   result.success = true
-  result.links = html.css('a').map { |anchor_tag| anchor_tag['href'] }
-  result.assets = html.css('img').map { |image_tag| image_tag['src'] }
+  result.links = html.css('a').map { |anchor_tag| anchor_tag['href'] }.filter { |link| valid_url?(link) }
+  result.assets = html.css('img').map { |image_tag| image_tag['src'] }.filter { |link| valid_url?(link) }
 
   result
+end
+
+def valid_url?(url)
+  url.start_with?('http') && url =~ URI::regexp
 end
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -19,6 +19,17 @@ RSpec.describe 'fetch method' do
         links: ['https://google.com', 'https://bing.com']
       )
     end
+
+    context 'when page has invalid links' do
+      let(:html) { "<a href='#'>Google</a><a href='https://bing.com'>Bing</a>" }
+
+      it 'returns only valid links' do
+        is_expected.to have_attributes(
+          success: true,
+          links: ['https://bing.com']
+        )
+      end
+    end
   end
 
   describe 'returns assets on page' do
@@ -30,6 +41,20 @@ RSpec.describe 'fetch method' do
         success: true,
         assets: ['https://google.com', 'https://bing.com']
       )
+    end
+
+    context 'when page has invalid links' do
+      let(:html) do
+        "<img src='data:image/svg+xml;base64,PHN2ZyiLz4='>Google</img>"\
+        "<img src='https://bing.com'>Bing</img>"
+      end
+
+      it 'returns only valid links' do
+        is_expected.to have_attributes(
+          success: true,
+          assets: ['https://bing.com']
+        )
+      end
     end
   end
 end
